3aa0e2e190f193e746c140c7b843cb49765e8990
DJConnect <pcvantol@hotmail.com>
2026-08-25 18:20:55 +0200

    test(engineering): use ten Playwright CI workers

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   fullyParallel: true,
-  workers: process.env.CI ? 8 : undefined,
+  workers: process.env.CI ? 10 : undefined,
   snapshotPathTemplate: "{testDir}/{testFilePath}-snapshots/{arg}-{platform}{ext}",
 });
